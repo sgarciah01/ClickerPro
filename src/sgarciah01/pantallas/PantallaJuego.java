a06509a3fe7263fbeed8aea8949f7f21cee1f7ea
Sergio García <sergiogarcia2893@gmail.com>
2020-01-23 13:45:59 +0100

    Panel izquierdo

--- src/sgarciah01/pantallas/PantallaJuego.java
+++ src/sgarciah01/pantallas/PantallaJuego.java
@@ -8,6 +8,7 @@ import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 import javax.imageio.ImageIO;
 
@@ -18,7 +19,7 @@ public class PantallaJuego implements Pantalla {
 	// ***** CONSTANTES ***** //
 	public final static int OPCIONES = 6;
 
-	/** FONDO **/
+	/** FONDO, IMÁGENES **/
 	private BufferedImage fondo;
 	private Image fondoEscalado;
 	private Image fondoLateralEscalado;
@@ -26,18 +27,31 @@ public class PantallaJuego implements Pantalla {
 	private Image iconoDefensa;
 	private Image iconoVida;
 	private Image iconoPersonaje;
+	private Image iconoDinero;
+	private Image iconoMas;
+	private Image iconoPocion;
 	
 	private Image iconoSpiderman1;
 	private Image iconoSpiderman2;
-	
-	/** VARIABLE GIF **/
-	private boolean flagGif = true;;
-	
+		
 	/** PANEL JUEGO **/
 	private PanelJuego panelJuego;
 
 	/** FUENTE **/
 	final Font fuenteSuperior = new Font("", Font.BOLD, 30);
+	final Font fuenteDinero = new Font("", Font.PLAIN, 20);
+	final Font fuenteLateralIzquierda = new Font("", Font.PLAIN, 25);
+	
+	/** VARIABLES PARA TIEMPO **/
+	private double tiempoInicial = 0;
+	private DecimalFormat formato = new DecimalFormat("#.##");
+	final Font fuenteTiempo = new Font("", Font.BOLD, 30);
+	
+	/** VARIABLES DEL VALOR DE LOS DATOS DEL PERSONALE **/
+	private int vida;
+	private int ataque;
+	private int defensa;
+	private int dinero;
 	
 	
 	/**
@@ -54,6 +68,14 @@ public class PantallaJuego implements Pantalla {
 		this.panelJuego = panelJuego;
 		
 		// Imágenes
+		obtencionYReescaladoImagenes(panelJuego);
+	}
+
+	/**
+	 * Obtiene todas las imágenes y las reescala.
+	 * @param panelJuego Panel de juego
+	 */
+	private void obtencionYReescaladoImagenes(PanelJuego panelJuego) {
 		try {
 			fondo = ImageIO.read(new File("img/fondo1.png"));
 			fondoLateralEscalado = ImageIO.read(new File("img/fondo_lateral.jpg"));
@@ -62,6 +84,9 @@ public class PantallaJuego implements Pantalla {
 			iconoVida = ImageIO.read(new File("img/corazon.png"));
 			iconoSpiderman1 = ImageIO.read(new File("img/spiderman1.png"));
 			iconoSpiderman2 = ImageIO.read(new File("img/spiderman2.png"));
+			iconoDinero = ImageIO.read(new File("img/moneda.png"));
+			iconoMas = ImageIO.read(new File("img/mas.png"));
+			iconoPocion = ImageIO.read(new File("img/pocion.png"));
 		} catch (IOException e1) {
 			e1.printStackTrace();
 			System.out.println("PROBLEMAS AL CARGAR LAS IMÁGENES. FIN DEL PROGRAMA");
@@ -80,8 +105,14 @@ public class PantallaJuego implements Pantalla {
 				iconoSpiderman1.getHeight(null), 80), 80, Image.SCALE_SMOOTH);
 		iconoSpiderman2 = iconoSpiderman2.getScaledInstance(getAnchoEscalado(iconoSpiderman2.getWidth(null), 
 				iconoSpiderman2.getHeight(null), 80), 80, Image.SCALE_SMOOTH);
+		iconoDinero = iconoDinero.getScaledInstance(getAnchoEscalado(iconoDinero.getWidth(null), 
+				iconoDinero.getHeight(null), 25), 25, Image.SCALE_SMOOTH);
+		iconoMas = iconoMas.getScaledInstance(getAnchoEscalado(iconoMas.getWidth(null), 
+				iconoMas.getHeight(null), 30), 30, Image.SCALE_SMOOTH);
+		iconoPocion = iconoPocion.getScaledInstance(getAnchoEscalado(iconoPocion.getWidth(null), 
+				iconoPocion.getHeight(null), 30), 30, Image.SCALE_SMOOTH);
 		fondoLateralEscalado = fondoLateralEscalado.getScaledInstance(250, 
-				panelJuego.getHeight() / OPCIONES, Image.SCALE_SMOOTH);
+				panelJuego.getHeight() / OPCIONES, Image.SCALE_SMOOTH);		
 		
 		iconoPersonaje = iconoSpiderman1;
 	}
@@ -107,22 +138,33 @@ public class PantallaJuego implements Pantalla {
 		
 		// ***** PANEL CENTRAL ***** //		
 		// Iconos superiores
-		g.setColor(new Color(147, 158, 117, 200));
+		g.setColor(new Color(147, 158, 117, 200));	// Fondo verde transparente
 		g.fillRect(250, 0, 500, 80);
 		g.drawImage(iconoAtaque, 270, 20, null);
 		g.drawImage(iconoDefensa, 445, 20, null);
 		g.drawImage(iconoVida, 600, 20, null);
 		
-		g.setColor(new Color(218, 239, 159));
+		g.setColor(new Color(218, 239, 159));	// Color letra
+		
 		g.setFont(fuenteSuperior);
-		g.drawString("40", 330, 50);
-		g.drawString("40", 505, 50);
-		g.drawString("100", 660, 50);
+		g.drawString(String.valueOf(ataque), 330, 50);
+		g.drawString(String.valueOf(defensa), 505, 50);
+		g.drawString(String.valueOf(vida), 660, 50);
+		
+		// Zona del dinero
+		g.setColor(new Color(147, 158, 117, 200));	// Fondo verde transparente
+		g.fillRect(250, 80, 150, 40);
+		
+		g.setFont(fuenteDinero);
+		g.setColor(Color.WHITE);
+		g.drawImage(iconoDinero, 280, 90, null);
+		g.drawString(String.valueOf(dinero), 310, 108);
 		
 		// Icono personaje
 		g.drawImage(iconoPersonaje, 460, 350, null);
 		
 
+		// FONDOS DE LOS PANELES LATERALES //
 		int altoCelda = panelJuego.getHeight() / OPCIONES;
 		int posYCelda;
 		
@@ -136,18 +178,37 @@ public class PantallaJuego implements Pantalla {
 		}
 
 		// Panel izquierdo
+		int posYItem;
+		String [] textos = {"Mejorar Ataque", "Mejorar Defensa", "Mejorar Vida Máxima",
+				"Tomar una Poción", "Generación Monedas", 
+				"Índice Crítico"};
 		
+		g.setFont(fuenteDinero);
+		g.setColor(new Color(194, 255, 246));
+		
+		// Iconos y textos
+		for (int i=0; i<3; i++) {
+			posYCelda = i * altoCelda;
+			posYItem = posYCelda + (altoCelda/2) - 15;
+			g.drawImage(iconoMas, 15, posYItem, null);
+			g.drawString(textos[i], 50, posYItem+22);			
+		}
+						
+		posYCelda = 3 * altoCelda;
+		posYItem = posYCelda + (altoCelda/2) - 15;
+		g.drawImage(iconoPocion, 15, posYItem, null);
+		g.drawString(textos[3], 50, posYItem+22);
+		
+		for (int i=4; i<6; i++) {
+			posYCelda = i * altoCelda;
+			posYItem = posYCelda + (altoCelda/2) - 15;
+			g.drawImage(iconoMas, 15, posYItem, null);
+			g.drawString(textos[i], 50, posYItem+22);			
+		}
 
+		
 		// Panel derecho
 		
-	}
-	
-	/**
-	 * Rellena el fondo de la pantalla con la imagen reescalada.
-	 * @param g Gráficos
-	 */
-	private void rellenarFondo(Graphics g) {
-		g.drawImage(fondoEscalado, 0, 0, null);
 	}
 
 	@Override
@@ -180,3 +241,4 @@ public class PantallaJuego implements Pantalla {
 	}
 
 }
+ 
